5b2c97ef94876184bdf2435d73e2d0f003cb345b
Alena Sharai <exultantislupus@gmail.com>
2023-11-06 11:20:49 +0400

    add App component, add location state and controlled value in input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return;
+    this.state = { location: '' };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  fetchWeather() {
+    console.log('Loading data...');
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <h1>Classy Weather</h1>
+        <div>
+          <input
+            type='text'
+            placeholder='Search for location...'
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          ></input>
+        </div>
+        <button onClick={this.fetchWeather}>Get weather</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
